d03e5152eb825fd2f0ee30057a36a79ec219ff7e
Farman Ahmed <farmanahmed888@gmail.com>
2024-11-22 18:01:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SPEProject
+ SPE Project
---
+++ interviewer-service/src/main/java/com/hackerrank/interviewer_service/InterviewerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hackerrank.interviewer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerServiceApplication.class, args);
+	}
+
+}
