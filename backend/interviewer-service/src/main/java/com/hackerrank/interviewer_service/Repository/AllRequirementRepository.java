fee83894b263759c7c1b577c3099adf9a668809c
Farman Ahmed <farmanahmed888@gmail.com>
2024-11-28 01:59:33 +0530

    fixed backend and interview frontend

--- backend/interviewer-service/src/main/java/com/hackerrank/interviewer_service/Repository/AllRequirementRepository.java
+++ backend/interviewer-service/src/main/java/com/hackerrank/interviewer_service/Repository/AllRequirementRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface AllRequirementRepository extends JpaRepository<AllRequirements, Integer> {
+public interface AllRequirementRepository extends JpaRepository<AllRequirements, Long> {
     Optional<AllRequirements> findByRequirementName(String requirementName);
 }
